750e86a37927a1065ae4cd3b94924720b0b86292
aqmoo <aqm2368@rit.edu>
2026-06-05 03:43:05 -0400

    EQ shader adjustments

--- p5/courseSketch.js
+++ p5/courseSketch.js
@@ -1,9 +1,12 @@
 let shaders;
 let squares = [];
-let size = 124; let cols = window.screen.width / size; let rows = window.screen.height / size;
+let initSize = 124 * 0.9;
+let size = initSize; let cols = window.screen.width / size; let rows = window.screen.height / size;
 let side1;
 let startDepth = -100;
 let mouseRange = 80;
+let horizontalShift = -(cols * size)/2 + size * (cols % 1) / 2;
+let verticalShift = -(rows * size)/2 + size * (rows % 1) / 2;
 
 function preload() {
     shaders = loadShader(
@@ -18,7 +21,7 @@ function setup() {
     rectMode(CENTER);
     angleMode(DEGREES);
 
-    size = 124 * 1.25 / (window.devicePixelRatio);
+    size = initSize * 1.25 / (window.devicePixelRatio);
 
     for(let i = 0; i < cols; i++) {
         squares[i] = [];
@@ -40,6 +43,8 @@ function draw() {
     shaders.setUniform("size", size);
     // shaders.setUniform("cameraZ", cam.eye.z);
     background(0);
+    horizontalShift = -(cols * size)/2 + size * (cols % 1) / 2;
+    verticalShift = -(rows * size)/2 + size * (rows % 1) / 2;
     for(let i = 0; i < cols; i++) {
         for ( let j = 0; j < rows; j++) {
             // squares[i][j].move();
@@ -52,7 +57,7 @@ function draw() {
 function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
     // cols = window.innerWidth / size; rows = window.innerHeight / size;
-    size = 124 * 1.25 / (window.devicePixelRatio);
+    size = initSize * 1.25 / (window.devicePixelRatio);
     for(let i = 0; i < cols; i++) {
         for(let j = 0; j < rows; j++) {
             squares[i][j].x = size * i - (cols * size)/2 + size * (cols % 1) / 2;
@@ -63,8 +68,6 @@ function windowResized() {
 
 class Rectangle {
     constructor(x, y, i, j) {
-        let horizontalShift = (cols * size)/2 - size * (cols % 1) / 2;
-        let verticalShift = (rows * size)/2 + size * (rows % 1) / 2;
         this.x = x;
         this.y = y;
         this.i = i;
@@ -73,20 +76,18 @@ class Rectangle {
     }
 
     display() {
-        let horizontalShift = -(cols * size)/2 + size * (cols % 1) / 2;
-        let verticalShift = -(rows * size)/2 + size * (rows % 1) / 2;
         // texture(side1);
         // noFill();
         let color = ('#00e1ff');
         color = 'black';
         // stroke(color);
-        noStroke();
-        // strokeWeight(0.5);
-        push();
+        //noStroke();
+        strokeWeight(0.7);
+        // push();
         if((mouseX - width/2 - this.x * 1.00)*(mouseX - width/2 - this.x * 1.00) + (mouseY - height/2 - this.y)*(mouseY - height/2 - this.y) < mouseRange*mouseRange) {
-            this.z = lerp(this.z, startDepth + size * 2 - size, 0.05);
+            this.z = lerp(this.z, startDepth + size * 2 - size, 0.1);
 
-            let t = 0.3;
+            let t = 0.4;
 
             let indexC = (this.i + 1) % squares.length;
             let indexR = this.j; 
@@ -103,11 +104,18 @@ class Rectangle {
             squares[indexC][indexR].z = lerp(squares[indexC][indexR].z, startDepth + size * 2 - size, t);;
         } else {
             this.z = lerp(this.z, startDepth + (size * 2 * noise(this.x - horizontalShift, this.y - verticalShift, millis()/4000)) - size, 0.5);
-            this.z = lerp(this.z, squares[(this.i + 1) % squares.length][this.j].z, 0.1);
-            this.z = lerp(this.z, squares[(this.i - 1 + squares.length) % squares.length][this.j].z, 0.1);
-            this.z = lerp(this.z, squares[this.i][(this.j + 1) % squares[this.i].length].z, 0.1);
-            this.z = lerp(this.z, squares[this.i][(this.j - 1 + squares[this.i].length) % squares[this.i].length].z, 0.1);
+            this.z = lerp(
+                this.z,
+                (
+                    squares[(this.i + 1) % squares.length][this.j].z + 
+                    squares[(this.i - 1 + squares.length) % squares.length][this.j].z +
+                    squares[this.i][(this.j + 1) % squares[this.i].length].z + 
+                    squares[this.i][(this.j - 1 + squares[this.i].length) % squares[this.i].length].z
+                ) / 4, 
+                0.6
+            );
         }
+        push();
         translate(this.x, this.y, this.z);
         box(size, size, size * 2);
         pop();
